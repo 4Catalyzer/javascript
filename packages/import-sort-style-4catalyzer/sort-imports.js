71cac95ee389d7ca50312aa20dd5a1a298a5e94d
Jason Quense <monastic.panic@gmail.com>
2017-08-10 13:25:27 -0400

    Add ignore option

--- packages/import-sort-style-4catalyzer/sort-imports.js
+++ packages/import-sort-style-4catalyzer/sort-imports.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
+const minimatch = require('minimatch');
 const { sortImports } = require('import-sort');
 const parser = require('import-sort-parser-babylon');
 const yargs = require('yargs');
@@ -12,9 +13,21 @@ const style = require('./index.js');
 const cwd = process.cwd();
 const argv = yargs
   .usage('$0 <glob> [args]')
+  .array('ignore')
   .help()
   .argv;
 
+// used as a reference:
+// https://github.com/babel/babel/blob/master/packages/babel-core/src/util.js
+const ignoreFiles = (argv.ignore || []).map((val) => {
+  let cleanVal = val;
+  // remove starting wildcards or relative separator if present
+  if (val.startsWith('./') || val.startsWith('*/')) cleanVal = val.slice(2);
+  if (val.startsWith('**/')) cleanVal = val.slice(3);
+
+  const regex = minimatch.makeRe(cleanVal, { nocase: true });
+  return new RegExp(regex.source.slice(1, -1), 'i');
+});
 
 const files = argv._.reduce((a, pattern) => (
   a.concat(glob.sync(`${pattern}/**/*js`, { absolute: true }))
@@ -22,6 +35,8 @@ const files = argv._.reduce((a, pattern) => (
 
 
 files.forEach((file) => {
+  if (ignoreFiles.some(r => !!file.match(r))) return;
+
   const source = fs.readFileSync(file, 'utf8');
 
   const { code, changes } = sortImports(
