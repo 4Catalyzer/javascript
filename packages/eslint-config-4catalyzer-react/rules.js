eb6297ce7bdc2971dd33f677d987ff365ae1590f
Jimmy Jia <tesrin@gmail.com>
2018-05-05 22:37:11 -0400

    Tweak some a11y rules

--- packages/eslint-config-4catalyzer-react/rules.js
+++ packages/eslint-config-4catalyzer-react/rules.js
@@ -1,5 +1,20 @@
 module.exports = {
-  'import/first': 'off', // We put all styling imports at the end.
+  // We put all styling imports at the end.
+  'import/first': 'off',
+  // Allow <Link to>.
+  // TODO: Remove this when airbnb includes this in next release.
+  'jsx-a11y/anchor-is-valid': ['error', {
+    components: ['Link'],
+    specialLink: ['to'],
+    aspects: ['noHref', 'invalidHref', 'preferButton'],
+  }],
+  'jsx-a11y/label-has-for': ['error', {
+    components: [],
+    required: {
+      some: ['nesting', 'id'],
+    },
+    allowChildren: false,
+  }],
   // `object` is used everywhere in Relay, and we use `any` deliberately.
   'react/forbid-prop-types': ['error', {
     forbid: ['array'],
