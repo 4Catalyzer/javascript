43825ab560bebb2256f3407403bf33b6c4a6497d
Jason Quense <monastic.panic@gmail.com>
2019-01-15 11:13:24 -0500

    feat: add typescript config. move babel-eslint to deps

---
+++ packages/eslint-config-4catalyzer-typescript/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  settings: {
+    'import/extensions': ['.js', '.ts', '.tsx'],
+    'import/parsers': {
+      [require.resolve('typescript-eslint-parser')]: ['.ts', '.tsx'],
+      // needed for typescript files importing js, since the configured parser
+      // will be ts not babel-eslint for the host file
+      [require.resolve('babel-eslint')]: ['.js'],
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: require.resolve('eslint-plugin-typescript/parser'),
+      parserOptions: {
+        sourceType: 'module',
+      },
+      plugins: ['typescript'],
+      rules: require('./rules'),
+    },
+    {
+      files: ['**/*.d.ts'],
+      rules: {
+        'import/no-duplicates': 'off',
+        'import/export': 'off',
+      },
+    },
+  ],
+};
