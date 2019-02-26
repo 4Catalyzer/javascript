66c5c20df01807517dbc7a7e5582e065fdb97729
Jimmy Jia <tesrin@gmail.com>
2019-02-25 21:54:22 -0500

    feat: Add test exclusion to Flow preset (#49)

--- packages/eslint-config-4catalyzer-flow/index.js
+++ packages/eslint-config-4catalyzer-flow/index.js
@@ -2,4 +2,13 @@ module.exports = {
   extends: ['plugin:flowtype/recommended', '4catalyzer'],
   plugins: ['flowtype'],
   rules: require('./rules'),
+
+  overrides: [
+    {
+      files: ['**/*.test.*', '**/test/**'],
+      rules: {
+        'flowtype/require-valid-file-annotation': 'off',
+      },
+    },
+  ],
 };
