b2f88db10aa47a10c30aa765621e5a27028af242
Jimmy Jia <tesrin@gmail.com>
2019-03-19 12:04:07 -0400

    feat: Add more convenience things in linting

--- packages/eslint-config-4catalyzer-jest/rules.js
+++ packages/eslint-config-4catalyzer-jest/rules.js
@@ -1,12 +1,23 @@
+let hasReact = true;
+try {
+  // eslint-disable-next-line
+  require('eslint-plugin-react');
+} catch (e) {
+  hasReact = false;
+}
+
 module.exports = {
   'global-require': 'off',
   'no-await-in-loop': 'off',
   'no-console': 'off',
   'import/no-dynamic-require': 'off',
+  'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   'jest/no-disabled-tests': 'warn',
   'jest/no-focused-tests': 'error',
   'jest/no-identical-title': 'error',
   'jest/prefer-to-have-length': 'warn',
   'jest/valid-expect': 'error',
-  'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  ...(hasReact && {
+    'react/prop-types': 'off',
+  }),
 };
--- packages/eslint-config-4catalyzer/index.js
+++ packages/eslint-config-4catalyzer/index.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['airbnb-base'],
   parser: require.resolve('babel-eslint'),
+  globals: {
+    __DEV__: false,
+  },
   rules: require('./rules'),
 };
