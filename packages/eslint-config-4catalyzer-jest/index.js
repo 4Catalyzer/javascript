426fcfdb6346f3fb56536b9fe640d4537ea49fe3
Jason Quense <monastic.panic@gmail.com>
2019-04-30 10:37:57 -0400

    feat: add __tests__ folders to jest config

--- packages/eslint-config-4catalyzer-jest/index.js
+++ packages/eslint-config-4catalyzer-jest/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   overrides: [
     {
-      files: ['**/*.test.*', '**/test/**'],
+      files: ['**/*.test.*', '**/test/**', '**/__test__/**'],
       plugins: ['jest'],
       env: {
         jest: true,
