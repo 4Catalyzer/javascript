de2e307eb25b062635cc0a6dd0676cb8d084edf7
Jimmy Jia <tesrin@gmail.com>
2019-08-14 17:51:44 -0400

    fix

--- packages/eslint-config-4catalyzer-react/index.js
+++ packages/eslint-config-4catalyzer-react/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   // airbnb needs to take precedence; otherwise rules from airbnb-base via
   // 4catalyzer messes up some JSX linting.
-  extends: ['4catalyzer', 'airbnb/hooks'],
+  extends: ['4catalyzer', 'airbnb', 'airbnb/hooks'],
   // We still want the 4catalyzer rules to take precedence over airbnb rules.
   rules: {
     ...require('eslint-config-4catalyzer/rules'),
