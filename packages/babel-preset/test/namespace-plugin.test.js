1b68948075540e3d687edfd02e00c3336a62d710
Jason Quense <monastic.panic@gmail.com>
2020-08-20 10:21:34 -0400

    feat: combine namespace plugin as intl option, upgrade to v8
    BREAKING CHANGE: intl plugin no longer extracts messages, must be done via the formatjs cli

--- packages/babel-preset/test/namespace-plugin.test.js
+++ packages/babel-preset/test/namespace-plugin.test.js
@@ -1,122 +1,117 @@
-import babelPluginTester from 'babel-plugin-tester';
+import { transformSync } from '@babel/core';
+import stripIndent from 'strip-indent';
 
-import namespacePlugin from '../namespace-plugin';
+import intlPreset from '../intl-preset';
 
-babelPluginTester({
-  plugin: namespacePlugin,
-  pluginName: 'namespacePlugin',
-  snapshot: false,
-  fixtures: false,
-  babelOptions: {
-    filename: 'foo.js',
-    presets: ['@babel/react'],
-  },
-  tests: {
-    'define messages': {
-      code: `
-          import { defineMessages } from 'react-intl';
+describe('intl tests', () => {
+  test.each([
+    [
+      'define messages',
+      {
+        code: `
+            import { defineMessages } from 'react-intl';
 
-          const messages = defineMessages({
-            title: {
-              id: 'title',
-            },
-            body: {
-              id: 'body',
-            },
-          });
+            const messages = defineMessages({
+              title: {
+                id: 'title',
+              },
+              body: {
+                id: 'body',
+              },
+            });
 
-          export { messages };
-        `,
-      output: `
-          import { defineMessages } from 'react-intl';
-          const messages = defineMessages({
-            title: {
-              id: '@4c/babel-preset:title',
-            },
-            body: {
-              id: '@4c/babel-preset:body',
-            },
-          });
-          export { messages };
-        `,
-    },
-
-    FormattedMessages: {
-      code: `
-          import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
-
-          <FormattedMessage id="title" />;
-          <FormattedHTMLMessage id="body" />;
-        `,
-      output: `
-          import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
-
-          /*#__PURE__*/
-          React.createElement(FormattedMessage, {
-            id: '@4c/babel-preset:title',
-          });
-
-          /*#__PURE__*/
-          React.createElement(FormattedHTMLMessage, {
-            id: '@4c/babel-preset:body',
-          });
-        `,
-    },
-    'intl.formatMessage': {
-      code: `
-          const intl = useIntl()
-
-          intl.formatMessage({
-            id: "title"
-          })
-        `,
-      output: `
-          const intl = useIntl();
-          intl.formatMessage({
-            id: '@4c/babel-preset:title',
-          });
-        `,
-    },
-
-    'provided prefix': {
-      pluginOptions: {
-        prefix: 'cool-pkg',
+            export { messages };
+          `,
+        output: `
+            import { defineMessages } from 'react-intl';
+            const messages = defineMessages({
+              title: {
+                "id": "@4c/babel-preset:title"
+              },
+              body: {
+                "id": "@4c/babel-preset:body"
+              }
+            });
+            export { messages };
+          `,
       },
-      code: `
-          import { defineMessages } from 'react-intl';
+    ],
 
-          const messages = defineMessages({
-            title: {
-              id: 'title',
-            },
-          });
+    [
+      'FormattedMessages',
+      {
+        code: `
+            import { FormattedMessage } from 'react-intl';
 
-          export { messages };
-        `,
-      output: `
-          import { defineMessages } from 'react-intl';
-          const messages = defineMessages({
-            title: {
-              id: 'cool-pkg:title',
-            },
-          });
-          export { messages };
-        `,
-    },
-    'quoted strings': {
-      code: `
-          const intl = useIntl()
+            <FormattedMessage id="title" />;
+          `,
+        output: `
+            import { FormattedMessage } from 'react-intl';
 
-          intl.formatMessage({
-            "id": "title"
-          })
-        `,
-      output: `
-          const intl = useIntl();
-          intl.formatMessage({
-            id: '@4c/babel-preset:title',
-          });
-        `,
-    },
-  },
+            /*#__PURE__*/
+            React.createElement(FormattedMessage, {
+              id: "@4c/babel-preset:title"
+            });
+
+          `,
+      },
+    ],
+    [
+      'intl.formatMessage',
+      {
+        options: {
+          extractFromFormatMessageCall: true,
+        },
+        code: `
+            const intl = useIntl()
+
+            intl.formatMessage({
+              id: "title"
+            })
+          `,
+        output: `
+            const intl = useIntl();
+            intl.formatMessage({
+              "id": "@4c/babel-preset:title"
+            });
+          `,
+      },
+    ],
+
+    [
+      'provided prefix',
+      {
+        options: {
+          prefix: 'cool-pkg',
+        },
+        code: `
+            import { defineMessages } from 'react-intl';
+
+            const messages = defineMessages({
+              title: {
+                id: 'title',
+              },
+            });
+
+            export { messages };
+          `,
+        output: `
+            import { defineMessages } from 'react-intl';
+            const messages = defineMessages({
+              title: {
+                "id": "cool-pkg:title"
+              }
+            });
+            export { messages };
+          `,
+      },
+    ],
+  ])('%s', (name, { code, output, options = {} }) => {
+    const result = transformSync(stripIndent(code), {
+      filename: 'foo.js',
+      presets: ['@babel/react', [intlPreset, options]],
+    }).code;
+
+    expect(result.trim()).toEqual(stripIndent(output).trim());
+  });
 });
