cc4ea327ffeaebc1dfdb2cc7868578e01938a72e
Jason Quense <monastic.panic@gmail.com>
2021-06-03 11:40:31 -0400

    feat: bump deps (#687)
    * feat: add isolatedModules
    
    * feat: bump deps

--- packages/jest-preset/index.js
+++ packages/jest-preset/index.js
@@ -3,8 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': require.resolve('./transforms/javascript'),
     '^.+\\.(scss|less|css)$': require.resolve('./transforms/styles'),
-    '^(?!.*\\.(js|jsx|ts|tsx|css|scss|less|json)$)': require.resolve(
-      './transforms/file',
-    ),
+    '^(?!.*\\.(js|jsx|ts|tsx|css|scss|less|json)$)':
+      require.resolve('./transforms/file'),
   },
 };
