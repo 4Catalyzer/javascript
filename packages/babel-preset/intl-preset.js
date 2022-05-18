616ec3d8ea864c0c226b0f06362ca80f35ed7d98
Jason Quense <monastic.panic@gmail.com>
2022-05-18 14:07:10 -0400

    feat: finally upgrade to formatjs plugin (#720)
    BREAKING CHANGE: intl settings, prefix must be provided to be used, otherwise defaults to generating ids when not present

--- packages/babel-preset/intl-preset.js
+++ packages/babel-preset/intl-preset.js
@@ -1,14 +1,67 @@
-const intlPlugin = require('babel-plugin-react-intl').default;
+const { mkdirSync, writeFileSync } = require('fs');
+const path = require('path');
 
-const namespacePlugin = require('./namespace-plugin');
+const { interpolateName } = require('@formatjs/ts-transformer');
+const intlPlugin = require('babel-plugin-formatjs').default;
 
-module.exports = function reactIntlPreset(_, options = {}) {
-  const { prefix, ...rest } = options;
+module.exports = function intlPreset(_, options = {}) {
+  const {
+    workspaceRoot,
+    messagesDir,
+    prefix,
+    idInterpolationPattern = '[sha512:contenthash:base64:6]',
+    ...rest
+  } = options;
+
+  // If a prefix is provided use it _only_ when a message has an explicit ID, otherwise fall back on default generation
+  const overrideIdFn = prefix
+    ? (id, defaultMessage, description, filename) =>
+        id
+          ? `${prefix}:${id}`
+          : // same logic as the plugin
+            interpolateName(
+              { resourcePath: filename },
+              idInterpolationPattern,
+              {
+                content: description
+                  ? `${defaultMessage}#${description}`
+                  : defaultMessage,
+              },
+            )
+    : undefined;
 
   return {
     plugins: [
-      [namespacePlugin, { prefix }],
-      [intlPlugin, { extractSourceLocation: true, ...rest }],
+      [
+        intlPlugin,
+        {
+          extractSourceLocation: true,
+          idInterpolationPattern,
+          overrideIdFn,
+          ...rest,
+          onMsgExtracted(filename, messages) {
+            if (!messages?.length) return;
+
+            if (!filename.startsWith(workspaceRoot)) {
+              throw new Error(
+                `File "${filename}" is not under workspace root "${workspaceRoot}". ` +
+                  'Please configure workspaceRoot to be a folder that contains all files being extracted',
+              );
+            }
+
+            const { name, dir } = path.parse(
+              path.relative(workspaceRoot, filename),
+            );
+
+            mkdirSync(path.join(messagesDir, dir), { recursive: true });
+
+            writeFileSync(
+              path.join(messagesDir, dir, `${name}.json`),
+              JSON.stringify(messages, null, 2),
+            );
+          },
+        },
+      ],
     ],
   };
 };
